c6e86a9dcc1c18205c4911c1392ce47ba0c159ce
Wesley Falcão Andrade <wesleyunimed@outlook.com>
2022-03-17 16:21:25 -0300

    first commit

---
+++ src/app/animacoes/animacoes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animacoes',
+  templateUrl: './animacoes.component.html',
+  styleUrls: ['./animacoes.component.scss']
+})
+export class AnimacoesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
