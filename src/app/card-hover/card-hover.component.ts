de3eb3d5f557ce8b473b058ad10b6532ae24994d
Wesley Falcão Andrade <wesleyunimed@outlook.com>
2022-12-04 10:16:26 -0300

    .

--- src/app/card-hover/card-hover.component.ts
+++ src/app/card-hover/card-hover.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import VanillaTilt from 'vanilla-tilt'
 
 @Component({
   selector: 'app-card-hover',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardHoverComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit(): void {
+    VanillaTilt.init(document.querySelector('card') as any);
   }
 
 }
